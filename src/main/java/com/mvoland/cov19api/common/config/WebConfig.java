77b9a4fa7887e9596dd96f429387da835b86d39a
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-05 13:07:55 +0200

    CORS allow all

--- src/main/java/com/mvoland/cov19api/common/config/WebConfig.java
+++ src/main/java/com/mvoland/cov19api/common/config/WebConfig.java
@@ -1,8 +1,6 @@
 package com.mvoland.cov19api.common.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,17 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private Environment env;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        String origin = env.getProperty("CLIENT_URL", "unknown");
-        System.err.println("=================================================");
-        System.err.println(origin);
-        System.err.println("=================================================");
 
         registry.addMapping("/**")
-                .allowedOrigins(origin);
+                .allowedOrigins("*");
     }
 }
