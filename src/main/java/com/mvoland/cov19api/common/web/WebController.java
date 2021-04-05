cfa8bc1a569c70a1a72ae781658a5202c3ab6ec5
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-05 18:42:56 +0200

    Asciidoc pages as default landing page

--- src/main/java/com/mvoland/cov19api/common/web/WebController.java
+++ src/main/java/com/mvoland/cov19api/common/web/WebController.java
@@ -2,7 +2,9 @@ package com.mvoland.cov19api.common.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -11,8 +13,8 @@ public class WebController {
 
 
     @RequestMapping("/")
-    public String index(Model model) {
-        return "index";
+    public ModelAndView index(ModelMap model) {
+        return new ModelAndView("redirect:/docs/index.html", model);
     }
 
 
