8efeff67c3f394b6f24ae3fe8717dc49c5e1b678
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-05 13:07:29 +0200

    CORS config by env variable

--- src/main/java/com/mvoland/cov19api/common/config/WebConfig.java
+++ src/main/java/com/mvoland/cov19api/common/config/WebConfig.java
@@ -1,7 +1,8 @@
 package com.mvoland.cov19api.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,11 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${api.client.url}")
-    String origin;
+
+    @Autowired
+    private Environment env;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String origin = env.getProperty("CLIENT_URL", "unknown");
         System.err.println("=================================================");
         System.err.println(origin);
         System.err.println("=================================================");
